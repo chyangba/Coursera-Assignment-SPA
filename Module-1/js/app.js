dc76a65c07e2208553c98600da694cfd621ec9dc
chyangba <chyangbabj@gmail.com>
2016-11-13 15:46:22 -0500

    Update app.js

--- Module-1/js/app.js
+++ Module-1/js/app.js
@@ -1,52 +1,32 @@
-(function () {
-'use strict';
-
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.lunchItems = "";
-  $scope.message = "";
-  $scope.messageStyle = {};
-  $scope.inputStyle = {};
-
-  // Function to check if the provided string is empty or not
-  // Returns true for empty result
-  $scope.checkIfEmpty = function (string) {
-    if (string == "" || string == undefined) return true;
-    else return false;
-  };
-
-  // Function to count items provided in the string (comma-separated list)
-  $scope.countItems = function (list) {
-    var items = list.split(',');
-    var count = 0;
-    for(var i = 0; i < items.length; i++) {
-      // Check if value between commas is empty
-      // increment count only for non-empty values
-      if (!$scope.checkIfEmpty(items[i])) count++;
-    }
-    return count;
-  }
-
-  // Function triggered on clicking button
-  // Checks if the provided lunch items are too much
-  // Returns "Enjoy!" message for items less than or equal to 3
-  // Returns "Too much!" for items more than 3
-  $scope.checkIfTooMuch = function () {
-    if ($scope.checkIfEmpty($scope.lunchItems)) {
-      $scope.message = "Please enter data first";
-      $scope.messageStyle.colorClass = "red-font";
-      $scope.inputStyle.colorClass = "red-border";
-    } else {
-      $scope.messageStyle.colorClass = "green-font";
-      $scope.inputStyle.colorClass = "green-border";
-      if ($scope.countItems($scope.lunchItems) <= 3) $scope.message = "Enjoy!";
-      else $scope.message = "Too much!";
-    }
-  };
-
-}
-
-})();
+(function(){
+    'use strict';
+    angular.module('myApp', []).controller('lunchController', lunchController);
+    lunchController.$inject = ['$scope'];
+    function lunchController($scope){
+        $scope.checkItems = function(str){
+            if(!str || 0 === str.length){
+                $scope.resaultCheck = 'Please enter data first';
+                $scope.active = 'redFocus';
+                $scope.textColor = 'def';
+            }else{
+                $scope.active = 'greenFocus';
+                $scope.inputColor = 'red';
+                var strArrey = str.split('');
+                var count = 0;
+                for(var i = 0; i < strArrey.length; i++){
+                    if(strArrey[i] === ','){
+                        count++;
+                    }
+                }
+                if( count < 3){
+                    $scope.resaultCheck = "Enjoy!";
+                    $scope.textColor = 'green';
+                }else{
+                    $scope.resaultCheck = "Too much!";
+                    $scope.textColor = 'red';
+                    
+                }
+            }
+        };
+    };
+}());
